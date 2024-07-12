e9af4faecabfb8f3aedaf0510f771e3b7ffc86ff
vicendy04 <htvd1204@gmail.com>
2024-07-12 19:33:22 +0700

    style: css changes

--- forum_project/settings.py
+++ forum_project/settings.py
@@ -133,9 +133,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+# LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = os.getenv("LANGUAGE_CODE")
 
-TIME_ZONE = "UTC"
+# TIME_ZONE = "UTC"
+TIME_ZONE = os.getenv("TIME_ZONE")
 
 USE_I18N = True
 
